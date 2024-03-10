a13fcc13e44b4c7bc243a56fa4e83dee98a844f0
UZ Okan <okanux12@gmail.com>
2024-03-10 12:51:45 +0100

    update

--- index.js
+++ index.js
@@ -20,8 +20,14 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const preferredLanguages = req.headers['accept-language'];
+  const software = req.headers['user-agent']
+  res.json({
+    ipaddress: req.ip,
+    language: preferredLanguages,
+    software: software,
+  })
 });
 
 // listen for requests :)
